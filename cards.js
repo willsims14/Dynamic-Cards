412e90185bf1feb3fedab57a0310bdda96213694
William A Sims III <willsims14@gmail.com>
2017-01-25 13:45:35 -0600

    Added delete button.

---
+++ cards.js
@@ -0,0 +1,46 @@
+document.getElementById("createButton").addEventListener("click", createCard);
+
+
+
+
+
+
+
+
+
+
+
+
+function createCard(){
+	var newCardText = document.getElementById("userInput").value;
+	var cardContainer = document.getElementById("cardContainer");
+	var childrenCount = cardContainer.childElementCount;
+
+	var newString =`
+		<div class="col-lg-4 col-md-6 col-sm-12">	
+		<article class="card" id="card--${childrenCount}">
+			<h3>Card</h3>
+			<p id="cardParagraph--!">${newCardText}</p>
+			<p>ID: ${childrenCount}</p>
+			<input type="button" value="Delete" id="cardDelete--${childrenCount}">
+		</article>
+		</div>`;
+
+	var divElement = document.createElement("div");
+	divElement.innerHTML = newString;
+
+
+	cardContainer.appendChild(divElement);
+
+	console.log("Chlidren: ", childrenCount);
+
+	document.getElementById("cardDelete--" + childrenCount).addEventListener("click", deleteCard);
+}
+
+function deleteCard(event){
+	console.log("Card to Delete: ", event.target.id);
+
+	var cardToDelete = event.target.parentNode;
+
+	cardToDelete.parentNode.removeChild(cardToDelete);
+}
